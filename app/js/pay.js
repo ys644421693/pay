01eea4d5cb2c1219348f80407372f050a1f15fe1
linyulong <linyulong@baofoo.com>
2017-11-02 21:18:19 +0800

    评论页面

--- app/js/pay.js
+++ app/js/pay.js
@@ -1,4 +1,4 @@
-
+var dc = [];
 function record(index){
     var i =0;
     for( ;i <= index;i++){
@@ -13,4 +13,20 @@ function record(index){
 
 function amountChange(element){
     console.log($(element).val());
+}
+
+function discussChoose(value){
+    for(var i =0 ;i < dc.length;i++){
+        if(dc[i] == value){
+            $("#discuss-type-" + value).parent().removeClass("discuss-box-select");
+            $("#discuss-type-" + value).parent().addClass("discuss-box-select-n");
+            dc.splice(i,1);
+            return;
+        }
+    }
+    if(dc.length < 3){
+        dc.push(value);
+        $("#discuss-type-" + value).parent().removeClass("discuss-box-select-n");
+        $("#discuss-type-" + value).parent().addClass("discuss-box-select");
+    }
 }
